e10ebdbde53947c58cabd9e024bd0113fd65298c
Atit <atitacharya8@gmail.com>
2019-04-30 01:46:12 +0545

    first react page

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>this is the todolist app v1.0.0 it is the part of a react crash course</p>
+        </React.Fragment>
+    );
+};
+
+export default About;
